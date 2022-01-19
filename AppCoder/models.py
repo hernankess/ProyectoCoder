26d43bb52b5b97647f2c6e3ae3e11d37974ea1b5
Hernan Kesselman <hernankesselman@hotmail.com>
2022-01-18 21:32:43 -0300

    Punto seguro clase 19

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,27 +1,24 @@
-import email
-from operator import mod
-from pyexpat import model
-from statistics import mode
 from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    
-    nombre = models.CharField(max_length=40)
+
+    nombre=models.CharField(max_length=40)
     camada = models.IntegerField()
 
+
 class Estudiante(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.EmailField()
-    
+    nombre= models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    email= models.EmailField()
+
 class Profesor(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.EmailField()
-    profesion = models.CharField(max_length=30)
-    
+    nombre= models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    email= models.EmailField()
+    profesion= models.CharField(max_length=30)
+
 class Entregable(models.Model):
-    nombre = models.CharField(max_length=30)
-    fechadeentrega = models.DateField()
-    entregado = models.BooleanField
+    nombre= models.CharField(max_length=30)
+    fechaDeEntrega = models.DateField()  
+    entregado = models.BooleanField()
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+# from http.client import HTTPResponse
+# from django.shortcuts import render
+
+# # Create your views here.
+
+# def inicio(request):
+#     return HTTPResponse ("INICIO")
+
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from AppCoder.models import Curso
 
 # Create your views here.
+
+def curso(request):
+
+      curso =  Curso(nombre="Desarrollo web", camada="19881")
+      curso.save()
+      documentoDeTexto = f"--->Curso: {curso.nombre}   Camada: {curso.camada}"
+
+
+      return HttpResponse(documentoDeTexto)
+
+
+def inicio(request):
+    return HttpResponse ("Inicio")
+    #   return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return HttpResponse ("cursos")
+
+    #   return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return HttpResponse ("profesores")
+
+    #   return render(request, "AppCoder/profesores.html")
+
+
+def estudiantes(request):
+    return HttpResponse ("estudiantes")
+
+    #   return render(request, "AppCoder/estudiantes.html")
+
+
+def entregables(request):
+    return HttpResponse ("entregable")
+
+    #   return render(request, "AppCoder/entregables.html")
